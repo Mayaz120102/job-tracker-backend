0bf4550a138f85c50c0b7c91a276b25b542c2039
Mayaz120102 <abrarmayaz2002@gmail.com>
2026-09-01 00:27:15 +0600

    updated

--- src/backend/main.py
+++ src/backend/main.py
@@ -3,13 +3,23 @@ from fastapi import FastAPI
 from backend import models
 from backend.database import engine
 from backend.router import applications, auth
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 models.Base.metadata.create_all(bind=engine)
 app.include_router(applications.router)
 app.include_router(auth.router)
+
 
 @app.get("/")
 def hello():
